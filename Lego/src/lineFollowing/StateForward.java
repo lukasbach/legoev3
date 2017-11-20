3bd5c9f23473aed124ebfbe193f124433ec3b3b7
Johanna Thiemich <johanna.thiemich@student.kit.de>
2017-11-20 09:58:20 +0100

    Implemented detecting obstacle.

--- Lego/src/lineFollowing/StateForward.java
+++ Lego/src/lineFollowing/StateForward.java
@@ -1,6 +1,9 @@
 package lineFollowing;
 
 import lejos.hardware.Sound;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.navigation.DifferentialPilot;
 import main.State;
 import robotcontrol.PortNotDefinedException;
@@ -11,21 +14,35 @@ public class StateForward extends State {
 	private static int ACC = 4000;
 	private static int SPEED = 200;
 	private float intensity = 0;
+	private SampleProvider touch;
+	private EV3TouchSensor touchSensor;
 	 
 	public StateForward(LineFollowing stateMachine, DifferentialPilot pilot, Robot robot) {
 		this.stateMachine = stateMachine;
 		this.pilot = pilot;
 		this.robot = robot;
+		touchSensor = new EV3TouchSensor(SensorPort.S2);
+		touch = touchSensor.getTouchMode();
 	}
 	
 	@Override
 	public void run() throws PortNotDefinedException {
 		intensity = robot.sensors.getColor();
 		
+		if (getTouch()) {
+			stateMachine.changeState(LineFollowing.OBSTACLE);
+		}
 		//sensor sees black
 		if (intensity < 0.4f) {
 			stateMachine.changeState(LineFollowing.ROTATE);
 		}
+		
+	}
+	
+	private boolean getTouch() {
+		float[] sample = new float[touch.sampleSize()];
+		touch.fetchSample(sample, 0);
+		return (sample[0] == 0) ? false : true;
 	}
 
 	@Override
