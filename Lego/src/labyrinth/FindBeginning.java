f9720e89cf368e7ea793637cb9c7073fd0ceedda
Johanna Thiemich <johanna.thiemich@student.kit.de>
2017-12-04 12:32:46 +0100

    Added find beginning stage

---
+++ Lego/src/labyrinth/FindBeginning.java
@@ -0,0 +1,52 @@
+package labyrinth;
+
+import lejos.hardware.Sound;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.utility.Delay;
+import main.State;
+import robotcontrol.PortNotDefinedException;
+import robotcontrol.Robot;
+import robotcontrol.SensorWrapper;
+
+public class FindBeginning extends State {
+
+	final static int MOVE_ACCELERATION = 4000;
+	final static int MOVE_SPEED = 200;
+	
+	public FindBeginning(Labyrinth stateMachine, DifferentialPilot pilot, Robot robot) {
+		this.stateMachine = stateMachine;
+		this.pilot = pilot;
+		this.robot = robot;
+	}
+
+	@Override
+	public void init() {
+		System.out.println("find beginning");
+		pilot.setAcceleration(MOVE_ACCELERATION);
+		pilot.setTravelSpeed(MOVE_SPEED);
+		pilot.forward();
+	}
+
+	@Override
+	public void run() throws PortNotDefinedException {
+		if(robot.sensors.getColor() == SensorWrapper.COLOR_ID_BLUE) {
+		/*	pilot.stop();
+			pilot.setAcceleration(MOVE_ACCELERATION);
+			pilot.setTravelSpeed(MOVE_SPEED);
+			pilot.forward();
+			Delay.msDelay(100);*/
+			Sound.twoBeeps();
+		}
+		if (robot.sensors.getColor() == SensorWrapper.COLOR_ID_LINE) {
+			pilot.stop();
+			Sound.beepSequenceUp();
+			stateMachine.changeState(Labyrinth.FOLLOW_LINE);
+		}
+	}
+
+	@Override
+	public void leave() {
+		// TODO Auto-generated method stub
+	}
+
+}
--- Lego/src/labyrinth/Labyrinth.java
+++ Lego/src/labyrinth/Labyrinth.java
@@ -7,11 +7,17 @@ import robotcontrol.Robot;
 
 public class Labyrinth extends CourseSectionStateMachine {
 
+	public static final int FIND_BEGINNING = 0;
+	public static final int FOLLOW_LINE = 1;
+
 	@SuppressWarnings("deprecation")
 	public Labyrinth(Robot robot, DifferentialPilot pilot) {
 		super();
-		State[] states = new State[1];
-		states[0] = new FollowLine(this, pilot, robot);
+		State[] states = new State[2];
+		
+		//TODO: exchange states:
+		states[1] = new FollowLine(this, pilot, robot);
+		states[0] = new FindBeginning(this, pilot, robot);
 		//states[1] =
 		//states[2] =
 		// ...
